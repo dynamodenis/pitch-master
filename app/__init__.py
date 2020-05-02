06d6a3fa8e58bfcda7bdbf449a93cf4d255f369a
Dynamo Denis <dmbugua66@gmail.com>
2020-05-02 15:39:03 +0300

    created the login and registration in the auth view

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,23 @@ from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 
 #create instance of bootstrap
 bootstrap=Bootstrap()
+#create database instance
 db=SQLAlchemy()
+#create loginmanager instance
+login_manager=LoginManager
+login_manager.session_protection='strong'
+login_manager.login_view='auth.login'
 def create_app(config_name):
 
     app=Flask(__name__)
     #initialize app extentions
     db.init_app(app)
     bootstrap.init_app(app)
+    login_manager.init_app(app)
     #regster your blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,14 +1,36 @@
-from flask import render_template
+from flask import render_template,redirect,url_for,request,flash
 from . import auth
 from .forms import LoginForm,RegistrationForm
 from .. import db
+from ..models import User
+from flask_login import login_user,logout_user,login_required
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
     form=LoginForm()
-    return render_template('auth/login.html',form=form)
+    if form.validate_on_submit:
+        user=User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user,form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or Password')
+
+    title='Login'
+    return render_template('auth/login.html',form=form,title=title)
 
 @auth.route('/register', methods=['GET','POST'])
 def register():
     register=RegistrationForm()
-    return render_template('auth/register.html',register=register)
+    if register.validate_on_submit():
+        user=User(username=register.username.data,email=register.email.data,password=register.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title="Create Account"
+    return render_template('auth/register.html',register=register,title=title)
+
+@auth.logout('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
